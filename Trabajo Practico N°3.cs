a9601d0de14f195877c84afe45b75d5f5312d6a4
santiagorodriguez912 <santiago.rodriguezet32@gmail.com>
2026-03-26 11:58:30 -0300

    Add files via upload

---
+++ Trabajo Practico N°3.cs
@@ -0,0 +1,106 @@
+﻿string ejercicio1(int n)
+{
+    string resultado;
+    if (n > 0)
+    {
+        resultado = "el numero es positivo";
+        return resultado;
+    }
+    else if (n < 0)
+    {
+        resultado = "el numero es negativo";
+        return resultado;
+    }
+    else
+    {
+        resultado= "es igual a 0";
+        return resultado;
+    }
+    
+}
+
+int n = convert.ToInt32(Console.ReadLine());
+string resultado = ejercicio1(n);
+Console.WriteLine(resultado);
+
+string ejercicio2(int edad)
+{
+    string fiesta;
+    if (edad >= 18)
+    {
+        fiesta = "¡Bienvenido a la fiesta!";
+        return fiesta;
+
+    } else if (edad >= 0)
+    {
+        fiesta = "Lo siento, eres muy joven";
+        return fiesta;
+    }else
+    {
+        fiesta = "edad no valida";
+        return fiesta;
+    }
+
+}
+
+int edad = convert.ToInt32(Console.ReadLine());
+string fiesta = ejercicio2(edad);
+Console.WriteLine(fiesta);
+
+string ejercicio3(string contra)
+{
+    string clave;
+    if ( contra == python123)
+    {
+        clave = " ¡Contraseña correcta! Acceso concedido.";
+        return clave;
+    }else
+    {
+        clave = "¡Contraseña incorrecta, Autodestrucción en 5 minutos!";
+        return clave;   
+    }
+
+}
+
+string contra = convert.ToString(Console.ReadLine());
+string clave = ejercicio3(contra);
+Console.WriteLine(clave);
+
+string ejercicio4(int numpar)
+{
+    string resuldiv;
+    if ( numpar%2 = 0) 
+    {
+        resuldiv = "El numero es par.";
+        return resuldiv;
+    }else
+    {
+        resuldiv = "El numero es impar.";
+        return resuldiv;
+    }
+
+}
+
+int contra = convert.ToInt(Console.ReadLine());
+string resuldiv = ejercicio4(numpar);
+Console.WriteLine(resuldiv);
+
+string ejercicio5(int edading)
+{
+    string regalo;
+    bool pochoclos = false;
+    if (edading > 65 && pochoclos == true)
+    {
+        regalo = "¡Felicidades! Tienes entrada gratuita al cine.";
+        return regalo;
+    }else
+    {
+        regalo = "Compra la entrada o raja de acá";
+        return regalo;
+    }
+
+}
+int edading = convert.ToInt(Console.ReadLine());
+string regalo = ejercicio5(numpar);
+Console.WriteLine(regalo);
+
