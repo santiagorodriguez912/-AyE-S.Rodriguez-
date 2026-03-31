34a59a64b915d8adf7ac99c0da4990bd552835c8
santiagorodriguez912 <santiago.rodriguezet32@gmail.com>
2026-03-31 13:10:54 -0300

    Add files via upload

---
+++ Trabajo Practico N°4 Estructuras For.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Ejercicio 1: Contando hasta 10");
+for (int a = 1; a <= 10; a++)
+{
+    Console.WriteLine( a );
+    
+}
+Console.WriteLine("Ejercicio 2: Hola, 5 veces...");
+for (int x= 0; x <= 5; x++)
+{
+    Console.WriteLine("¡Hola, mundo!.");
+
+}
+Console.WriteLine("Ejercicio 3: Los números pares");
+for (int par = 2; par <= 20; par += 2)
+{
+    Console.WriteLine(par);
+}
+Console.WriteLine("Ejercicio 4: Tabla de multiplicar del 7");
+for (int tab = 7; tab <=70; tab+=7)
+{
+    Console.WriteLine(tab);
+}
+Console.WriteLine("Ejercicio 5: Suma de los primeros 5 números");
+int suma = 0;
+for (int asd = 1; asd <= 5; asd++)
+{
+    suma += asd;
+}
+Console.Write("1+2+3+4+5 = ");
+Console.Write(suma);
+
