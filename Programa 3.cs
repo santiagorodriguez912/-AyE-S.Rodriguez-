75494a13fdb705ac41fd136d5bb3cd1148e3baa0
santiagorodriguez912 <santiago.rodriguezet32@gmail.com>
2026-05-05 11:56:09 -0300

    Add files via upload

---
+++ Programa 3.cs
@@ -0,0 +1,49 @@
+﻿// EJERCICIO PROGRAMA 3 S.RODRIGUEZ
+
+string palabra = "MANTECA";
+string aux = "";
+int intentos = 7;
+for (int i = 0; i < palabra.Length; i++)
+{
+    aux += "_";
+}
+while (intentos > 0 && aux != palabra)
+{
+    Console.WriteLine("Palabra: " + aux);
+    Console.WriteLine("Intentos restantes: " + intentos);
+    Console.WriteLine("Ingrese una letra: ");
+    string letra = Console.ReadLine().ToUpper();
+
+    bool asd = false;
+    string nv = "";
+
+    for (int i = 0; i < palabra.Length; i++)
+    {
+        if (palabra[i].ToString() == letra)
+        {
+            nv += letra;
+            asd = true;
+        }
+        else
+        {
+           nv += aux[i];
+        }
+    }
+
+    aux = nv;
+
+    if (asd == false)
+    {
+        intentos--;
+        Console.WriteLine("Letra incorrecta");
+    }
+
+}
+if (aux == palabra)
+{
+    Console.WriteLine("ganaste, La palabra era: " + palabra);
+}
+else
+{
+    Console.WriteLine("perdiste, La palabra era: " + palabra);
+}
