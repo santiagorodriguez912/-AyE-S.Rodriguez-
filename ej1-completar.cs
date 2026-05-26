196a7d5fc0c7200e3eef3d948d4e0f55f0b78efc
santiagorodriguez912 <santiago.rodriguezet32@gmail.com>
2026-05-26 13:14:43 -0300

    Add files via upload

---
+++ ej1-completar.cs
@@ -0,0 +1,29 @@
+﻿//ejercicio 1
+List<string> nombres = new List<string>()
+{
+    "Marlon",
+    "Tramontana",
+    "Aura_guler",
+    "Paralelepipedo",
+    "Umamiamor",
+    "adolfo",
+    "Calampa",
+    "VivanLosGitanos",
+    "Kirchnerista",
+    "GitHubMiPasion",
+    "SoyMoro",
+    "CalleParaSiempre",
+    "menganito",
+    "Oreo",
+    "OsoMaduro",
+
+};
+string Palabralarga = "";
+for (int i = 0; i < nombres.Count; i++)
+{
+    if (nombres[i].Length > Palabralarga.Length)
+    {
+        Palabralarga = nombres[i];
+    }
+}
+Console.WriteLine("la palabra mas larga es: " + Palabralarga);
